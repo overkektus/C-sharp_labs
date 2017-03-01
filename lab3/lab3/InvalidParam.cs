2cd12e383847d64ceb376494525d78e911a491d6
overkektus <egor.piskunou@gmail.com>
2017-03-01 20:32:31 +0300

    laboratory work num 3

---
+++ lab3/lab3/InvalidParam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class InvalidParam: Exception
+    {
+        private string message;
+
+        public InvalidParam(string message)
+        {
+            this.message = message;
+        }
+
+        public string What()
+        {
+            return message;
+        }
+    }
+}
---
+++ lab3/lab3/Shaman_p2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    sealed partial class Shaman
+    {
+        public override void WriteToFile()
+        {
+            File.WriteAllText("shaman.txt", "========Fighter type: " + WhatType());
+            File.AppendAllText("shaman.txt", "\r\n~~~Level: " + Level.ToString());
+            File.AppendAllText("shaman.txt", "\r\n~~~Dexterity: " + Dexterity.ToString());
+            File.AppendAllText("shaman.txt", "\r\n~~~HP: " + HP.ToString());
+            File.AppendAllText("shaman.txt", "\r\n~~~Damage: " + Damage.ToString());
+            File.AppendAllText("shaman.txt", "\r\n~~~Mana: " + Mana.ToString());
+        }
+    }
+}
